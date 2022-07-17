9cecf2387087794f17bc47e7bd5a8eb9ddd3cfe7
amangilev <artmnglv@gmail.com>
2022-07-17 12:21:07 +0500

    Add build for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, 'docs'),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/jsonmap-repl/'
+    : '/'
 })
